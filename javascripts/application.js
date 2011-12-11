989a2e0bc20d06d74dce4d491aac6109a46cf694
David Anguita <davidbanguita@gmail.com>
2011-12-11 11:16:35 +0100

    Updated JS output (CoffeeScript)

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,23 +1,22 @@
-$(document).ready(function() {
-
-  // Twitter timeline JSON wrapper
-  $("#twitter-json-wrapper").tweet({
-    username: "danguita",
-    join_text: "-",
-    avatar_size: false,
-    count: 8,
-    loading_text: false
+(function() {
+  $(function() {
+    var initialize;
+    initialize = function() {
+      $("#twitter-json-wrapper").tweet({
+        username: "danguita",
+        join_text: "-",
+        avatar_size: false,
+        count: 8,
+        loading_text: false
+      });
+      $("#delicious-json-wrapper").delicious("danguita", {
+        type: "posts",
+        favicon: false
+      });
+      return $("#feed-wrapper").rssfeed("http://feeds.feedburner.com/DavidAnguita", {
+        limit: 3
+      });
+    };
+    return initialize();
   });
-
-  // Delicious bookmarks JSON wrapper
-  $('#delicious-json-wrapper').delicious("danguita", {
-    type: "posts",
-    favicon: false
-  });
-
-  // Feed wrapper
-  $('#feed-wrapper').rssfeed("http://feeds.feedburner.com/DavidAnguita", {
-    limit: 3
-  });
-
-});
+}).call(this);
